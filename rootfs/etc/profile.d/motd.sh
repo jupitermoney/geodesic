7a5eaf20a5abd9453df3ebde41e82e00200dbea5
Rohit Verma <rohit.verma@niki.ai>
2019-02-18 11:30:10 +0530

    removed groff, making hooks slow.

--- rootfs/etc/profile.d/motd.sh
+++ rootfs/etc/profile.d/motd.sh
@@ -1,7 +1,7 @@
 if [ -z "${ASSUME_ROLE}" ]; then
 	if [ -f "/etc/motd" ]; then
 		cat "/etc/motd"
-		if [ -z "${ENABLE_K8_NOTIFICATION}" ]; then
+		if [ "${ENABLE_K8_NOTIFICATION}"=="true" ]; then
 			kubectl get pods --all-namespaces | grep -E 'CrashLoopBackOff|Pending|Error'
 		fi
 	fi
