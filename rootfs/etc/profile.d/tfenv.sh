e5a1a82865d4e4927b90c13a7d3679d626d5b894
Rohit Verma <rohit.verma@niki.ai>
2019-02-25 18:59:56 +0530

    removed groff, making hooks slow.

--- rootfs/etc/profile.d/tfenv.sh
+++ rootfs/etc/profile.d/tfenv.sh
@@ -3,12 +3,17 @@
 #source <(tfenv)
 export TF_VAR_namespace="niki"
 export TF_VAR_region="${AWS_REGION}"
-export TF_VAR_region="${AWS_REGION}"
 export TF_VAR_account_id="${AWS_ACCOUNT_ID}"
 export TF_BUCKET_REGION="${AWS_REGION}"
-export TF_DYNAMODB_TABLE="${TF_VAR_namespace}-${TF_VAR_stage}-terraform-state-lock"
-export TF_VAR_domain_name="${TF_VAR_stage}.niki.ai"
 export TF_VAR_gitlab_token="${GITLAB_ACCESS_TOKEN}"
 export TF_CLI_PLAN_PARALLELISM=2
 export MAKE_INCLUDES="Makefile Makefile.*"
+export TF_VAR_parent_domain_name="niki.ai"
+export TF_VAR_stage="${STAGE}"
+export TF_VAR_domain_name="${TF_VAR_stage}.niki.ai"
 export TF_VAR_aws_assume_role_arn="arn:aws:iam::${TF_VAR_account_id}:role/${ASSUME_ROLE}"
+export TF_DYNAMODB_TABLE="${TF_VAR_namespace}-${TF_VAR_stage}-terraform-state-lock"
+export TF_BUCKET="${TF_VAR_namespace}-${TF_VAR_stage}-terraform-state"
+
+# Filesystem entry for tfstate
+s3 fstab "${TF_BUCKET}" "/" "/secrets/tf"
