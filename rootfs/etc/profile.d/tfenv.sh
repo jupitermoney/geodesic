b0546b5bcfa58317b7e368a59841c82819570c2b
Rohit Verma <rohit.verma@niki.ai>
2019-03-04 23:07:38 +0530

    added motd

--- rootfs/etc/profile.d/tfenv.sh
+++ rootfs/etc/profile.d/tfenv.sh
@@ -18,4 +18,4 @@ export TF_BUCKET="${TF_VAR_namespace}-${TF_VAR_stage}-terraform-state"
 # Filesystem entry for tfstate
 s3 fstab "${TF_BUCKET}" "/" "/secrets/tf"
 s3 mount
-export KUBECONFIG="/conf/eks/kubeconfig_${TF_VAR_domain_name}"
+export KUBECONFIG=$(echo "/conf/eks/kubeconfig_${TF_VAR_domain_name}" | sed -e "s/\./_/g")
